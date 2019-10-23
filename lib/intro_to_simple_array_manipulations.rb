8a1395ac10baee42442cb12d8af3e9487e190b5b
Sean Good <tree662@protonmail.com>
2019-10-23 19:44:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,8 +22,8 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat(array1, array2)
-  array2.concat(array1)
+def using_concat(my_favorite_things, more_favs)
+  more_favs.concat(my_favorite_things)
 end
 
 def using_insert
